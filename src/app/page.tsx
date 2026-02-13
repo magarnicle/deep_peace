9ffcaf2f11c0d3d9efd48594b3b1381cb7d7d1ac
Matthew Rademaker <matthew@acctv.com.au>
2026-02-13 13:29:09 +1000

    put an ad in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,11 +87,11 @@ function AdUnit() {
     <div style={{ textAlign: "center", marginTop: "20px" }}>
       <ins
         className="adsbygoogle"
-        style={{ display: "block" }}
+        style={{ display: "block", textAlign: "center" }}
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
         data-ad-client="ca-pub-9728124941305322"
-        data-ad-slot="XXXXXXXXXX"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
+        data-ad-slot="9996660811"
       />
     </div>
   );
