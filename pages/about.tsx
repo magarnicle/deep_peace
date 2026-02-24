3466b5fee3a7f10b7400e5bab3afb7b6a8b6751e
Matthew Rademaker <matthew@acctv.com.au>
2026-02-24 19:24:23 +1000

    about page

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+// 'use client';
+
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>About Us</h1>
+      <p>Welcome to the About Page of our Next.js Application!</p>
+      <p>Hereou can learn more about what we do.</p>
+          </div>
+            );
+};
+
+export default About;
